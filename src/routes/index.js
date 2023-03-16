0c3b9e121c7ea57c2acee30e604a6e9a39d81b32
Isaac Bruno Quiroga <isaacbrunosecu@gmail.com>
2023-03-15 22:30:52 -0300

    agrega lo necesario para que funcione

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,18 @@
 const express = require('express');
 // const users = require('../api/users/routes');
 // const issues = require('../api/issues/routes');
-const notebooks = require('../api/notebooks/routes');
+// const notebooks = require('../api/notebooks/routes');
 // const addresses = require('../api/addresses/routes');
 // const addressTypes = require('../api/addressTypes/routes');
 
 const router = express.Router();
 
+router.use('/', (request, response) => {
+  console.log('Probando...')
+  response.send("hola que tal")
+})
 // router.use('/users', users);
-router.use('/notebooks', notebooks);
+// router.use('/notebooks', notebooks);
 // router.use('/addresses', addresses);
 // router.use('/addressTypes', addressTypes);
 // router.use('/issues', issues);
