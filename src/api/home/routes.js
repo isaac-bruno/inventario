cda320903a7290b76e3143fed6cb61224df5c048
Isaac Bruno Quiroga <isaacbrunosecu@gmail.com>
2023-03-17 21:24:58 -0300

    crea la pagina principal

--- src/api/home/routes.js
+++ src/api/home/routes.js
@@ -3,7 +3,14 @@ const router = express.Router()
 
 router.get('/', (requests, response) => {
   response.render('home.hbs', {
-    title: "titulo grandioso"
+    title: "titulo grandioso",
+    links: [
+      {name: 'Notebooks', url: 'notebooks'},
+      {name: 'Usuarios', url: 'users'},
+      {name: 'Direcciones', url: 'addresses'},
+      {name: 'Asuntos', url: 'issues'},
+      {name: 'Tipos de direcciones', url: 'addressTypes'},
+    ]
   })
 })
 
