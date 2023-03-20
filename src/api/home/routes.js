082b56670567103134ecd08a7bc52fb8786b7b93
Isaac Bruno Quiroga <isaacbrunosecu@gmail.com>
2023-03-20 19:30:15 -0300

    elimina los link pasados por la ruta

--- src/api/home/routes.js
+++ src/api/home/routes.js
@@ -2,16 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (requests, response) => {
-  response.render('home.hbs', {
-    title: "titulo grandioso",
-    links: [
-      {name: 'Notebooks', url: 'notebooks'},
-      {name: 'Usuarios', url: 'users'},
-      {name: 'Direcciones', url: 'addresses'},
-      {name: 'Asuntos', url: 'issues'},
-      {name: 'Tipos de direcciones', url: 'addressTypes'},
-    ]
-  })
+  response.render('home.hbs')
 })
 
 module.exports = router
