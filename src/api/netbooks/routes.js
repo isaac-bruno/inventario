f778ef08a47003814cc0b40847011ea62f1c2925
Isaac Bruno Quiroga <isaacbrunosecu@gmail.com>
2023-03-20 19:44:57 -0300

    agrega un modal de prueba

--- src/api/netbooks/routes.js
+++ src/api/netbooks/routes.js
@@ -3,7 +3,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (request, response) => {
-  response.send("hola mundo")
+  response.render('netbooks/index.hbs')
 })
 
 module.exports = router
